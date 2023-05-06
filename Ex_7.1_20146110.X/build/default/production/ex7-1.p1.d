60e6b28080788d80796dca403b72497169432a73
Nguyen Hai Dang <79005118+DannyNguyen24-Vietnam@users.noreply.github.com>
2023-05-07 00:27:52 +0700

    Add files via upload

---
+++ Ex_7.1_20146110.X/build/default/production/ex7-1.p1.d
@@ -0,0 +1,3 @@
+build/default/production/ex7-1.p1:  \
+ex7-1.c  \
+16F887_config.h 
---
+++ Ex_7.2_20146110.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Ex_7.2_20146110.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Ex_7.2_20146110.X.production.hex
